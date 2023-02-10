c3d0cb6dfa5351459a0f87b3f55b217aca927fc6
Nico <nicoyang.mason@gmail.com>
2023-02-10 16:01:35 +0800

    1.0.1

---
+++ SnowBrowser/GADHelper.swift
@@ -0,0 +1,8 @@
+//
+//  GADHelper.swift
+//  SnowBrowser
+//
+//  Created by yangjian on 2023/1/16.
+//
+
+import Foundation
---
+++ SnowBrowser/GADNativeView.swift
@@ -0,0 +1,8 @@
+//
+//  GADNativeView.swift
+//  SnowBrowser
+//
+//  Created by yangjian on 2023/1/16.
+//
+
+import Foundation
